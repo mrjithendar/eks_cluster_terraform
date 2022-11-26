756cc934968f150070a10a9856b0c3a6e46d5733
jithendar <jithendardharmapuri@gmail.com>
2022-11-26 14:42:56 +0530

    cluster code added

--- main.tf
+++ main.tf
@@ -1,29 +1,50 @@
-###### root/main.tf
-module "eks" {
-  source                  = "./modules/eks"
-  aws_public_subnet       = module.vpc.aws_public_subnet
-  vpc_id                  = module.vpc.vpc_id
-  cluster_name            = "module-eks-${random_string.suffix.result}"
-  endpoint_public_access  = true
-  endpoint_private_access = false
-  public_access_cidrs     = ["0.0.0.0/0"]
-  node_group_name         = "luit22"
-  scaling_desired_size    = 1
-  scaling_max_size        = 1
-  scaling_min_size        = 1
-  instance_types          = ["t3.small"]
-  key_pair                = "XXXX"
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
 }
 
-module "vpc" {
-  source                  = "./modules/vpc"
-  tags                    = "luit22"
-  instance_tenancy        = "default"
-  vpc_cidr                = "10.0.0.0/16"
-  access_ip               = "0.0.0.0/0"
-  public_sn_count         = 2
-  public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
-  map_public_ip_on_launch = true
-  rt_route_cidr_block     = "0.0.0.0/0"
+#S3 backend
+terraform {
+  backend "s3" {
+    bucket = "jithendar"
+    key    = "tf_statefiles/ek_terraform/eks_cluster.tfstate"
+    region = "us-east-1"
+  }
+}
 
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+provider "kubernetes" {
+  cluster_ca_certificate = base64decode(module.eks.kubeconfig-certificate-authority-data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
 }
